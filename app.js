58e6d4db0e918b966c3991a364dd431e1b4c0d1c
mree <m9lzl0v@gmail.com>
2017-02-18 22:33:11 -0800

    added css file, modified index with button updated app.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 // On first click make event listener red
+
 // const myHeading = document.getElementById('myHeading');
-//
 // myHeading.addEventListener('click', () => {
 //   myHeading.style.color = 'red';
 // });
 
-
-
 // On click alternate between red and black
 const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
 
-myHeading.addEventListener('click', () => {
+myButton.addEventListener('click', () => {
   if (myHeading.style.color != 'red') {
       myHeading.style.color = 'red';
   }
